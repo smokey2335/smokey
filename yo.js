b66884e1b31f50d4012bd5ffbc8bf39b9558da2a
smokey2335 <116642185+smokey2335@users.noreply.github.com>
2023-04-10 17:52:52 +0200

    Update yo.js

--- yo.js
+++ yo.js
@@ -1,33 +1,4 @@
-(function (window)  {
-    'use strict';
-     var
-         userAgent  = navigator.userAgent.toLowerCase(),
-         thisUrl = window.location.href,
-          referrerUrl = document.referrer,
-         baseName  = 'plug',
-         counter = 0,
-          cookieName = "_plug",
-         makeInterval  = 1 , //minutes
-         maxCap  = gtp["raw"], // base but overwritten
-          keepCookie = 60*24, // minutes
-          trafficType = gtp["traffic"],
-         mobile  = {
-              true: /iphone|ipad|android|ucbrowser|iemobile|ipod|blackberry|bada/.test(userAgent)
-         },
-          browser = {
-              win: /windows/.test(userAgent),
-              mac: /macintosh/.test(userAgent),
-              mobile: /iphone|ipad|ucbrowser|iemobile|ipod|blackberry|bada/.test(userAgent),
-              android:  /android/.test(userAgent),
-              ios:  /iphone|ipad|ipod/.test(userAgent),
-              webkit: /webkit/.test(userAgent),
-              mozilla: /mozilla/.test(userAgent) &&  !/(compatible|webkit)/.test(userAgent),
-              chrome: /chrome/.test(userAgent),
-              msie: /msie|trident\//.test(userAgent) && !/opera/.test(userAgent),
-              firefox: /firefox/.test(userAgent),
-              safari:  /safari/.test(userAgent) && !/chrome/.test(userAgent),
-              opera: /opera/.test(userAgent),
-              version: parseInt(userAgent.match(/(?:[^\s]+(?:ri|ox|me|ra)\/|trident\/.*?rv:)([\d]+)/i)[1], 10)
-          },
-         targetElement = null,  // base but overwritten
-         url  = "https://www.smokeyandbash.com/2023/03/interstitial.html",
+if(typeof under!=="undefined"&&under!==null){if(typeof cta!=="undefined"&&cta!==null){document.addEventListener('click',function(event){var target=event.target;while(target){if(target.tagName==='A'&&target.classList.contains(cta)){const linkHref=target.href;event.preventDefault();window.open(linkHref,'_blank');top.location.replace(under);break;}
+target=target.parentNode;}});}else{document.addEventListener('click',function(event){var target=event.target;while(target){if(target.tagName==='A'){const linkHref=target.href;event.preventDefault();window.open(linkHref,'_blank');top.location.replace(under);break;}
+target=target.parentNode;}});}}
+if(typeof back!=="undefined"&&back!==null){!(function(){var t;try{const URL=window.location.href.split(/[#]/)[0];for(t=0;10>t;++t)history.pushState({},'',URL+'#');onpopstate=function(event){event.state&&top.location.replace(back);};}catch(o){console.log(o);}})();}
